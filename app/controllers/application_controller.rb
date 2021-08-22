b4c4a8c17e30fb2b6dcabf40239835410873d38b
satoudd <88134639+satoudd@users.noreply.github.com>
2021-08-22 18:49:46 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_permitted_parameters, if: :devise_contoroller?
+
+  private
+  def configure_parameter_santizer.permit(:sing_up, keys: [:name])
+ end
 end
