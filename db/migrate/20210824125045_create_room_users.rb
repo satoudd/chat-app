7da1b8246a5c6b4a16e4693fb4fe11add402e662
satoudd <88134639+satoudd@users.noreply.github.com>
2021-08-24 23:02:54 +0900

    room_userテーブルの作成

--- db/migrate/20210824125045_create_room_users.rb
+++ db/migrate/20210824125045_create_room_users.rb
@@ -1,8 +1,9 @@
-class CreateRooms < ActiveRecord::Migration[6.0]
+class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :rooms do |t|
+    create_table :room_users do |t|
       t.references :room, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
+      t.timestamps
     end
   end
 end
